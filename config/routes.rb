50b7e92aa0a667ef502dc02128c1f0dd0a82df74
Jonathan Rudd <jonathanrudd3@gmail.com>
2022-07-02 16:47:18 +0900

    complete but uglee

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,18 @@ Rails.application.routes.draw do
   # See all
   root to: 'tasks#index'
   get 'tasks', to: 'tasks#index', as: 'tasks'
-  # See details
-  get 'tasks/:id', to: 'tasks#show'
+
   # create
-  get 'tasks/new', to: 'tasks#new', as: 'tasks_new'
-  post 'tasks/new', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new', as: 'new'
+  post 'tasks', to: 'tasks#create'
+
+  # See details
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
   # update
-  get 'tasks/:id/:edit', to: 'tasks#edit', as: 'tasks_edit'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
   patch 'tasks/:id', to: 'tasks#update'
+
   # destroy
   delete 'tasks/:id', to: 'tasks#destroy'
 end
