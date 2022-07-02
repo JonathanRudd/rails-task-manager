dd0cb78a10af0dfe2446c6f482e78180ca22a094
Jonathan Rudd <jonathanrudd3@gmail.com>
2022-07-02 14:38:50 +0900

    all method fin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # See all
+  root to: 'tasks#index'
+  get 'tasks', to: 'tasks#index', as: 'tasks'
+  # See details
+  get 'tasks/:id', to: 'tasks#show'
+  # create
+  get 'tasks/new', to: 'tasks#new', as: 'tasks_new'
+  post 'tasks/new', to: 'tasks#create'
+  # update
+  get 'tasks/:id/:edit', to: 'tasks#edit', as: 'tasks_edit'
+  patch 'tasks/:id', to: 'tasks#update'
+  # destroy
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
